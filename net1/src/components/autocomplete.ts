75613febdd96740ffa1d14cc11ccdacf6ef07591
Lucien Taylor <lucien.taylor@oxil.co.uk>
2020-03-19 18:09:18 +0000

    various

---
+++ net1/src/components/autocomplete.ts
@@ -0,0 +1,101 @@
+import Configuration from "../../../../kinicart/kiniauth-js/ts/configuration";
+
+/**
+ * Reusable autocomplete components
+ */
+export default class Autocomplete extends HTMLElement {
+
+  // Constructor
+  constructor() {
+    super();
+    this.init();
+  }
+
+  // Initialiser
+  private init() {
+
+    let input = this.querySelector("input");
+    let resultsDiv = <HTMLElement>this.querySelector("[data-results]");
+
+    let keyCounter = 0;
+
+    // Listen for input on the text field
+    input.addEventListener("input", (event) => {
+
+      let me = <HTMLInputElement>event.target;
+
+      // Increment global key counter
+      keyCounter++;
+
+      let myCounter = keyCounter;
+
+      setTimeout(() => {
+
+        if (myCounter == keyCounter) {
+          this.processInput();
+        }
+      }, 500);
+
+    });
+
+    // Process special keys
+    input.addEventListener("keydown", (event) => {
+
+      if (event.key == "Escape") {
+        input.blur();
+      }
+
+    });
+
+
+    // Process focus
+    input.addEventListener("focus", () => {
+      this.processInput();
+    });
+
+    // Process blur
+    input.addEventListener("blur", () => {
+      this.setResultVisibility(false);
+    });
+
+
+    // Stop propagation on clicks within this component
+    this.addEventListener("click", (event) => {
+      event.stopPropagation();
+    });
+
+    // Ensure we close the results on body clicks outside of our component
+    document.body.addEventListener("click", () => {
+      this.setResultVisibility(false);
+    });
+
+
+  }
+
+  // Process input
+  private processInput() {
+
+    let input = this.querySelector("input");
+    let minChars = this.getAttribute("data-min-chars") ? this.getAttribute("data-min-chars") : 2;
+
+    // Show / hide according to length of value.
+    this.setResultVisibility(input.value.length >= minChars);
+
+  }
+
+
+  // Set result visibility
+  private setResultVisibility(visible: boolean) {
+    let resultsDiv = <HTMLElement>this.querySelector("[data-results]");
+    Configuration.elementVisibilityFunction(resultsDiv, visible);
+
+    if (visible) {
+      this.dispatchEvent(new Event("resultsShow"));
+    } else {
+      this.dispatchEvent(new Event("resultsHide"));
+    }
+
+  }
+
+
+}
--- net1/src/net1.ts
+++ net1/src/net1.ts
@@ -1,23 +1,31 @@
 import "net1-base";
 import Kinicart from "../../../kinicart/kinicart-js/ts/index";
+import Autocomplete from "./components/autocomplete";
 
 export default class Net1 {
 
-    constructor(settings: any) {
-        this.init(settings);
-    }
+  constructor(settings: any) {
+    this.net1Init(settings);
+  }
 
-    private init(settings: any) {
+  private net1Init(settings: any) {
 
-        // Enable Netistrar White Label
-        new Kinicart({
-            endpoint: settings.backendUrl,
-            recaptchaKey: settings.recaptchaKey,
-            elementVisibilityFunction: function (element: Element, visible: boolean) {
-                element.setAttribute("data-state", visible ? "show" : "hide");
-            }
-        });
+    // Enable Netistrar White Label
+    new Kinicart({
+      endpoint: settings.backendUrl,
+      recaptchaKey: settings.recaptchaKey,
+      elementVisibilityFunction: this.elementVisibility
+    });
 
-    }
+    // Create any custom elements we require
+    customElements.define("net1-autocomplete", Autocomplete);
+
+  }
+
+  // Accessible from base classes.
+  protected elementVisibility(element: Element, visible: boolean) {
+    element.setAttribute("data-state", visible ? "show" : "hide");
+  }
+
 
 }
